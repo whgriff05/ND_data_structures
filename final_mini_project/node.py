6fd5474389860b6cf6d2bac00c4e7191fc7c3ec9
Will Griffin <willhgriffin@icloud.com>
2025-11-28 16:29:57 -0500

    Added get_child and add_child

--- final_mini_project/node.py
+++ final_mini_project/node.py
@@ -6,12 +6,13 @@ class Node:
     def has_children(self):
         return False if len(self.children.items()) == 0 else True
     
-    def __getitem__(self, key):
+    def get_child(self, key):
         if key not in self.children:
-            raise KeyError
+            return None
         
         return self.children[key]
     
-    def __setitem__(self, key, value):
-        self.children[key] = value
+    def add_child(self, k):
+        self.children[k] = Node(k)
+        
 
--- final_mini_project/tests/node_test.py
+++ final_mini_project/tests/node_test.py
@@ -24,3 +24,35 @@ class NodeTest(unittest.TestCase):
         self.assertEqual(n3.key, "b")
         self.assertNotEqual(n3.children, {})
         self.assertEqual(n3.children["a"].key, "a")
+
+    def test_node_has_children(self):
+        n1 = Node()
+        self.assertEqual(n1.key, None)
+        self.assertEqual(n1.children, {})
+        self.assertEqual(n1.has_children(), False)
+
+        n2 = Node("b")
+        n2.children["a"] = Node("a")
+        self.assertEqual(n2.key, "b")
+        self.assertNotEqual(n2.children, {})
+        self.assertEqual(n2.children["a"].key, "a")
+        self.assertEqual(n2.has_children(), True)
+
+    def test_node_get_child(self):
+        n1 = Node()
+        self.assertEqual(n1.get_child("a"), None)
+        self.assertEqual(n1.has_children(), False)
+
+        n2 = Node("a")
+        n1.children["a"] = n2
+        self.assertEqual(n1.get_child("a"), n2)
+        self.assertEqual(n1.has_children(), True)
+
+    def test_node_add_child(self):
+        n1 = Node("a")
+        self.assertEqual(n1.has_children(), False)
+
+        n1.add_child("b")
+        self.assertEqual(n1.get_child("b").key, "b")
+        self.assertEqual(n1.has_children(), True)
+
