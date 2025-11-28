d5aa87ef116b56cbdea3a071ae6e835d7f190c4e
Will Griffin <willhgriffin@icloud.com>
2025-11-28 16:33:11 -0500

    Initial Trie Test file

---
+++ final_mini_project/tests/trie_test.py
@@ -0,0 +1,11 @@
+import unittest
+from trie import Trie
+
+class NodeTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
