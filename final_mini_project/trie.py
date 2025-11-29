10a2fc3d7d47c23e3157b7a1ec6396ceca8df8ee
Will Griffin <willhgriffin@icloud.com>
2025-11-29 15:56:37 -0500

    added contains and add_word methods

--- final_mini_project/trie.py
+++ final_mini_project/trie.py
@@ -3,5 +3,33 @@ from node import Node
 class Trie:
     def __init__(self):
         self.head = Node("*")
+
+    def __contains__(self, word):
+        current_node = self.head
+        for letter in word:
+            if current_node.get_child(letter) is None:
+                return False
+            current_node = current_node.get_child(letter)
+
+        if current_node.get_child(0) is None:
+            return False
+
+        return True
+
+    def add_word(self, word):
+        if word not in self:
+            current_node = self.head
+            for letter in word:
+                if current_node.get_child(letter) is None:
+                    current_node.add_child(letter)
+
+                current_node = current_node.get_child(letter)
+
+            if current_node.get_child(0) is None:
+                current_node.add_child(0)
+
+            return True
+        else:
+            return False
         
 
