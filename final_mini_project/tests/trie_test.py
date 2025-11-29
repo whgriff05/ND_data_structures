101c7daaaa2c346ec2430f838dc18c8269b273ba
Will Griffin <willhgriffin@icloud.com>
2025-11-29 15:56:29 -0500

    Added init, contains, and add_word tests

--- final_mini_project/tests/trie_test.py
+++ final_mini_project/tests/trie_test.py
@@ -1,7 +1,8 @@
 import unittest
 from trie import Trie
+from node import Node
 
-class NodeTest(unittest.TestCase):
+class TrieTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pass
@@ -9,3 +10,42 @@ class NodeTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         pass
+
+    def test_trie_init(self):
+        t1 = Trie()
+        self.assertEqual(type(t1.head), type(Node()))
+        self.assertEqual(t1.head.key, "*")
+
+    def test_trie_contains(self):
+        t1 = Trie()
+        t1.head.add_child("b")
+        t1.head.get_child("b").add_child("u")
+        t1.head.get_child("b").get_child("u").add_child("s")
+        t1.head.get_child("b").get_child("u").get_child("s").add_child(0)
+
+        self.assertEqual(t1.head.key, "*")
+        self.assertEqual(t1.head.get_child("b").key, "b")
+        self.assertEqual(t1.head.get_child("b").get_child("u").key, "u")
+        self.assertEqual(t1.head.get_child("b").get_child("u").get_child("s").key, "s")
+        self.assertEqual(t1.head.get_child("b").get_child("u").get_child("s").get_child(0).key, 0)       
+
+        self.assertEqual("bus" in t1, True)
+        self.assertEqual("busy" in t1, False)
+
+    def test_trie_add_word(self):
+        t1 = Trie()
+        
+        self.assertEqual("bus" in t1, False)
+        self.assertEqual("rhombus" in t1, False)
+        self.assertEqual("busy" in t1, False)
+
+        self.assertEqual(t1.add_word("bus"), True)
+        self.assertEqual("bus" in t1, True)
+        self.assertEqual(t1.add_word("bus"), False)
+
+        self.assertEqual(t1.add_word("rhombus"), True)
+        self.assertEqual("rhombus" in t1, True)
+
+        self.assertEqual(t1.add_word("busy"), True)
+        self.assertEqual("busy" in t1, True)
+
