950f94217faa4c8a9ebd8f4fdbd07b029639ef46
Will Griffin <willhgriffin@icloud.com>
2025-12-05 18:03:53 -0500

    Data Structure Dependency

--- final_mini_project/spelling_checker.py
+++ final_mini_project/spelling_checker.py
@@ -3,7 +3,7 @@ import sys
 import time
 from trie import Trie
 
-def initialize_dictionary_trie(dict_file):
+def initialize_dictionary(dict_file, structure):
     dict_trie = Trie()
     with open(dict_file, "r") as df:
         for line in df:
@@ -17,7 +17,7 @@ def initialize_dictionary_trie(dict_file):
 
     return dict_trie
 
-def get_misspells_trie(dict_trie, text_file):
+def get_misspells(dict_trie, text_file):
     misspells = []
     with open(text_file, "r") as tf:
         for line in tf:
@@ -31,6 +31,7 @@ def get_misspells_trie(dict_trie, text_file):
                     misspells.append(word)
     return misspells
 
+
 def main(args=sys.argv):
     aarray = []
     for arg in args:
@@ -38,18 +39,29 @@ def main(args=sys.argv):
             continue
         aarray.append(arg)
 
-    if len(aarray) != 2:
+    if len(aarray) != 3:
         print("Error: too many arguments")
-        print("Usage: \t ./spelling_checker.py <dictionary file> <text file>")
+        print("Usage: \t ./spelling_checker.py -[t/d/a] <dictionary file> <text file>")
+        sys.exit(1)
+
+    if aarray[0] == "-t":
+        pass
+    elif aarray[0] == "-d":
+        pass
+    elif aarray[0] == '-a':
+        pass
+    else:
+        print("Error: data structure does not exist")
+        print("Usage: \t ./spelling_checker.py -[t/d/a] <dictionary file> <text file>")
         sys.exit(1)
 
     tstart = time.time()
-    dict_trie = initialize_dictionary_trie(aarray[0])
+    dict_trie = initialize_dictionary(aarray[1])
     t_initialize = round(time.time() - tstart, 5)
-    print(f"Time to initialize dict_tree: {t_initialize}")
+    print(f"Time to initialize word dictionary: {t_initialize}")
 
     tstart = time.time()
-    misspells = get_misspells_trie(dict_trie, aarray[1])
+    misspells = get_misspells(dict_trie, aarray[2])
     t_misspells = round(time.time() - tstart, 5)
     print(f"Time to search for misspells: {t_misspells}")
 
